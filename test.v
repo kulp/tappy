d0fe1559f82d25a845a866f3260b647c9a419a98
Darren Kulp <darren@kulp.ch>
2021-07-10 16:26:57 -0400

    Introduce push_{bit,byte}

--- test.v
+++ test.v
@@ -5,6 +5,24 @@ module test(output clk, dat);
 
     string logfile = "test.vcd";
 
+    reg clk = 1;
+    reg dat = 1;
+
+    // Bits are pushed out on posedge, and should be read on negedge.
+    task push_bit(input b);
+        #2 dat = b;
+        #2 clk = 0;
+        #4 clk = 1;
+    endtask
+
+    task push_byte(byte word);
+        integer i;
+        push_bit(0);
+        for (i = 0; i < 8; i = i + 1)
+            push_bit(word[i]);
+        push_bit(1);
+    endtask
+
     initial begin
         $display("Starting test");
         $dumpfile(logfile);
